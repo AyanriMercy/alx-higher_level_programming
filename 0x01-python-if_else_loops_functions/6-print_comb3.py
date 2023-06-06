a8bd540e7fd105cb440cb094302f576a6c47c649
ayanrimercy <ayanrimercy@gmail.com>
2023-06-06 17:53:17 +0000

    6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+for a in range(9):
+    for b in range(10):
+        if a < b:
+            if ((a*10) + b) != 89:
+                print("{}{}".format(a, b), end=", ")
+            else:
+                print("{}{}".format(a, b))
