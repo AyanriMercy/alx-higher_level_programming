077ba65e6ccad9a3412e771a02f266fb9e4fddd6
AyanriMercy <ayanrimercy@gmail.com>
2023-06-26 10:02:00 -0700

    0x05-python-exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,6 @@ def safe_print_list(my_list=[], x=0):
             print("{0}".format(my_list[i]), end="")
             total += 1
         except IndexError:
-            break
+            pass
     print()
-    return(total)
+    return total
